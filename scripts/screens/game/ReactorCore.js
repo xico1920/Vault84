62bff60a28d4290e9ed7d0f7870ca4815b712c07
Francisco Silva <beatzframe55@gmail.com>
2025-08-20 16:49:15 +0100

    video replacement

--- scripts/screens/game/ReactorCore.js
+++ scripts/screens/game/ReactorCore.js
@@ -3,16 +3,20 @@ export function createReactorCoreScreen() {
         async render() {
             return `
                 <div class="reactor-core-container grid grid-cols-12 gap-4">
-                <!-- Conteúdo por cima -->
+                    
+                    <!-- Conteúdo -->
                     <div class="reactor-content col-span-12 md:col-span-12">
                         <h1>REACTOR CORE</h1>
                         <p>The reactor is still not in use.</p>
                     </div>
-                <!-- Vídeo de fundo -->
-                    <video autoplay loop muted>
-                        <source src="./assets/Animation/reactor.webm" type="video/webm">
 
-                    </video>
+                    <!-- Vídeo ocupa 12 colunas -->
+                    <div class="col-span-4">
+                        <video autoplay loop muted class="w-full">
+                            <source src="./assets/Animation/reactor1.webm" type="video/webm">
+                        </video>
+                    </div>
+
                 </div>
             `;
         }
